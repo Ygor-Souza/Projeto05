3ab200936d0591364a5b8f9c30a2965f75de58d5
Ygor-Souza <ygorsilvad.souza@gmail.com>
2025-10-06 19:16:08 -0300

    Projeto 5 navegação

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import about from "./screens/about";
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+       <Drawer.Navigator
+        screenOptions={{
+          drawerStyle: {
+            backgroundColor: "#4c689eff",
+            width: "60%",
+          },
+          drawerActiveTintColor: "#b8eeffff",
+          drawerActiveBackgroundColor: "#213c71ff",
+          drawerInactiveTintColor: "#fff",
+        }}
+      >
+        <Drawer.Screen 
+          name="about"
+          component={about}
+          options={{
+            headerShown: false,
+            drawerItemStyle: {display : "none"},
+          }}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
